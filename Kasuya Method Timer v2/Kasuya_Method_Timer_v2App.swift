0c4f0c4b197101218bd57b4e0618461d6ffa201d
Jose Martin Ipong <joseipong@gmail.com>
2026-01-28 22:31:55 +0800

    Initial Commit

---
+++ Kasuya Method Timer v2/Kasuya_Method_Timer_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Kasuya_Method_Timer_v2App.swift
+//  Kasuya Method Timer v2
+//
+//  Created by Martin on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct Kasuya_Method_Timer_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Kasuya Method Timer v2Tests/Kasuya_Method_Timer_v2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Kasuya_Method_Timer_v2Tests.swift
+//  Kasuya Method Timer v2Tests
+//
+//  Created by Martin on 1/28/26.
+//
+
+import Testing
+@testable import Kasuya_Method_Timer_v2
+
+struct Kasuya_Method_Timer_v2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
